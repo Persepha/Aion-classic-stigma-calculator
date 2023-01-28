02c3f80ffc6ff24882d9f077ed6e540e024130f8
Vasilevich Dmitry <vasilevich.dmt@gmail.com>
2023-01-28 19:28:05 +0300

    Add CharacterLvlPicker component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
 import { Roboto } from "@next/font/google";
-import { useState } from "react";
+
 import { ClassesEnum, ClassesEnumType } from "@/utils/aionClasses/types";
 import { ClassesPanel } from "@/components/ClassesPanel";
+import { StigmaPanel } from "@/components/StigmaPanel";
+import {
+  getNumberAdvancedSlotsAllowed,
+  getNumberDefaultSlotsAllowed,
+} from "@/utils/slots_calc";
+import { CharLvlPicker } from "@/components/CharLvlPicker";
 
 const font = Roboto({
   weight: "500",
@@ -13,6 +20,18 @@ export default function Home() {
     ClassesEnum.FIGHTER
   );
 
+  const [characterLvl, setCharacterLvl] = useState<number>(55);
+
+  const [numberDefaultSlots, setNumberDefaultSlots] = useState<number>(6);
+  const [numberAdvancedSlots, setNumberAdvancedSlots] = useState<number>(5);
+
+  const [defaultStigmaSlots, setDefaultStigmaSlots] = useState([]);
+
+  useEffect(() => {
+    setNumberDefaultSlots(getNumberDefaultSlotsAllowed(characterLvl));
+    setNumberAdvancedSlots(getNumberAdvancedSlotsAllowed(characterLvl));
+  }, [characterLvl]);
+
   function selectClass(selectedClass: ClassesEnumType) {
     setCurrentClass(ClassesEnum[selectedClass]);
   }
@@ -21,6 +40,15 @@ export default function Home() {
     <>
       <main className={font.className}>
         <ClassesPanel selectClass={selectClass} selectedClass={currentClass} />
+        <CharLvlPicker
+          characterLvl={characterLvl}
+          setCharacterLvl={setCharacterLvl}
+          selectedClass={currentClass}
+        />
+        <StigmaPanel
+          numberAdvancedSlotsAllowed={numberAdvancedSlots}
+          numberDefaultSlotsAllowed={numberDefaultSlots}
+        />
       </main>
     </>
   );
