8d43a51a7da02d3d8a8509c66a14de1498772e9d
Vasilevich Dmitry <vasilevich.dmt@gmail.com>
2023-02-20 20:15:03 +0300

    Fix github-pages asserts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/Aion-classic-stigma-calculator",
+  assetPrefix: "/Aion-classic-stigma-calculator/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
